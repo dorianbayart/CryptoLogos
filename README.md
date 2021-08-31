db1673db366472f10114891c4f0b3980c2e9baaa
Dorian Bayart <dorianbayart@users.noreply.github.com>
2021-08-31 15:27:59 +0200

    Initialize the repo
    Project is usable
    All logos are updated
    Readme is in work in progress

---
+++ README.md
@@ -0,0 +1,60 @@
+
+
+
+# [CryptoLogos](https://github.com/dorianbayart/CryptoLogos)
+
+
+
+## What is CryptoLogs ?
+
+When you work with Web3 and blockchain's explorers, the only way to precisely identify a token is by its contract address.
+
+I created CryptoLogos because I couldn't find any useful library of logos easily and programmatically reachable by contract addresses.
+
+With CryptoLogos, you can grab logos of many crypto tokens directly from their explorer, and then use them easily with their contract addresses !
+
+
+
+## Where are the logos ?
+
+### Example
+_Problem_: you need to display the DAI logo of the polygon blockchain. You know the contract address is: [0x8f3cf7ad23cd3cadbd9735aff958023239c6a063](https://polygonscan.com/token/0x8f3cf7ad23cd3cadbd9735aff958023239c6a063)
+
+_Solution_: build the following URL with the chain name and the contract address: https://github.com/dorianbayart/CryptoLogos/tree/main/CryptoLogos/[chain]/[contract address].png?raw=true
+
+_Result_: ![DAI Logo](https://github.com/dorianbayart/CryptoLogos/tree/main/CryptoLogos/polygon/0x8f3cf7ad23cd3cadbd9735aff958023239c6a063.png?raw=true)
+
+
+
+## How to run the commands ?
+
+```
+npm install
+```
+
+```
+npm run start:*chain*
+```
+where *chain* can be one of those parameters:
+* ethereum
+* polygon
+* bsc
+* fantom
+* all
+
+
+
+## Supported blockchains
+
+* [x] [Ethereum](https://github.com/dorianbayart/CryptoLogos/tree/main/CryptoLogos/ethereum)
+* [x] [Polygon](https://github.com/dorianbayart/CryptoLogos/tree/main/CryptoLogos/polygon)
+* [x] [Binance Smart Chain](https://github.com/dorianbayart/CryptoLogos/tree/main/CryptoLogos/bsc)
+* [x] [Fantom](https://github.com/dorianbayart/CryptoLogos/tree/main/CryptoLogos/fantom)
+
+
+
+## Why do I have errors ?
+
+A lot of errors are due to the anti-DDOS of Cloudflare, wich prevents bots to send many requests.
+
+You can run the same command again to resume, already downloaded logos will be bypassed.
---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict'
+
+const cheerio = require('cheerio')
+const { curly } = require('node-libcurl')
+const fetch = require('node-fetch')
+const fs = require('fs')
+const makeDir = require('make-dir')
+const os = require('os')
+
+const SUBFOLDER = 'CryptoLogos'
+const INTERVAL = 1000
+
+const CHAINS = {
+  ETHEREUM: {
+    path: 'ethereum',
+    explorer: 'https://etherscan.io',
+    list: 'https://api.dexpairs.xyz/list'
+  },
+  POLYGON: {
+    path: 'polygon',
+    explorer: 'https://polygonscan.com',
+    list: 'https://api.dexpairs.xyz/sushiswap/list'
+  },
+  BSC: {
+    path: 'bsc',
+    explorer: 'https://bscscan.com',
+    list: 'https://api.dexpairs.xyz/pancake/list'
+  },
+  FANTOM: {
+    path: 'fantom',
+    explorer: 'https://ftmscan.com',
+    list: 'https://api.dexpairs.xyz/spiritswap/list'
+  }
+}
+
+
+const args = process.argv.slice(2)
+const chain = CHAINS[Object.keys(CHAINS).find(chain => CHAINS[chain].path === args[0])]
+
+let total = 0
+let imgDownloaded = 0
+let errors = 0
+let alreadyExists = 0
+
+fetch(chain.list)
+  .then(res => res.json())
+  .then(async (json) => {
+    const path = await makeDir(SUBFOLDER + '/' + chain.path)
+
+    if(Object.keys(json).length > 0) {
+      fs.writeFile(path + '/map.json', JSON.stringify(json), () =>
+        {}
+      )
+    } else {
+      json = JSON.parse(fs.readFileSync(path + '/map.json'))
+    }
+
+    total = Object.keys(json).length
+    Object.keys(json).forEach(async (tokenAddress, i) => {
+      tokenAddress = tokenAddress.toLowerCase()
+      if (!fs.existsSync(path + '/' + tokenAddress + '.png')) {
+        setTimeout(async function() {
+          // console.log(tokenAddress)
+          await curly.get(chain.explorer + '/token/' + tokenAddress)
+            .then(({ statusCode, data }) => data)
+            .then(async (html) => {
+              const $ = cheerio.load(html)
+              if($('img.u-sm-avatar').length === 0) {
+                console.log('No picture found in the page for: ' + tokenAddress)
+                return
+              }
+              const imgSrc = $('img.u-sm-avatar')[0].attribs.src
+              // console.log(imgSrc)
+
+              await curly.get(chain.explorer + imgSrc)
+                .then(({ statusCode, data }) => data)
+                .then(async (buffer) => {
+
+                  // console.log(path + '/' + tokenAddress + '.png')
+                  fs.writeFile(path + '/' + tokenAddress + '.png', buffer, () => {
+                    imgDownloaded += 1
+                    logStatus()
+                  }
+                  )
+                })
+            })
+            .catch(error => {
+              console.log(tokenAddress, error)
+              errors += 1
+              logStatus()
+            })
+        }, i * 1000)
+
+      } else {
+        // already exists
+        alreadyExists += 1
+        logStatus()
+      }
+    })
+
+  })
+
+function logStatus() {
+  console.log('Total: ' + total + ' | Already exists: ' + alreadyExists + ' | Downloaded: ' + imgDownloaded + ' | Errors: ' + errors)
+}
