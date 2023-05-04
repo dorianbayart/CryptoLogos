f8379fb914d249cff7be39c065b0113b1c1927eb
Dorian Bayart <dorianbayart@users.noreply.github.com>
2023-05-05 01:14:12 +0200

    Add Arbitrum support + Add more logos

--- index.js
+++ index.js
@@ -14,27 +14,32 @@ const CHAINS = {
   ETHEREUM: {
     path: 'ethereum',
     explorer: 'https://etherscan.io',
-    list: 'https://api.dexpairs.xyz/list'
+    list: 'https://api.dexpairs.xyz/eth/simple'
   },
   POLYGON: {
     path: 'polygon',
     explorer: 'https://polygonscan.com',
-    list: 'https://api.dexpairs.xyz/quickswap/list'
+    list: 'https://api.dexpairs.xyz/matic/simple'
   },
   BSC: {
     path: 'bsc',
     explorer: 'https://bscscan.com',
-    list: 'https://api.dexpairs.xyz/pancake/list'
+    list: 'https://api.dexpairs.xyz/bnb/simple'
   },
   FANTOM: {
     path: 'fantom',
     explorer: 'https://ftmscan.com',
-    list: 'https://api.dexpairs.xyz/spiritswap/list'
+    list: 'https://api.dexpairs.xyz/ftm/simple'
   },
   XDAI: {
     path: 'xdai',
     explorer: 'https://gnosisscan.io',
-    list: 'https://api.dexpairs.xyz/honeyswap/list'
+    list: 'https://api.dexpairs.xyz/gno/simple'
+  },
+  ARBITRUM_ONE: {
+    path: 'arbitrum_one',
+    explorer: 'https://arbiscan.io',
+    list: 'https://api.dexpairs.xyz/arb1/simple'
   }
 }
 
@@ -49,68 +54,66 @@ let alreadyExists = 0
 let timer = 0
 
 fetch(chain.list)
-.then(res => res.json())
-.then(async (json) => {
-  const path = await makeDir(SUBFOLDER + '/' + chain.path)
+  .then(res => res.json())
+  .then(async (json) => {
+    const path = await makeDir(SUBFOLDER + '/' + chain.path)
 
-  if(Object.keys(json).length > 0) {
-    fs.writeFile(path + '/map.json', JSON.stringify(json), () =>
-    {}
-  )
-} else {
-  json = JSON.parse(fs.readFileSync(path + '/map.json'))
-}
+    if(Object.keys(json).length > 0) {
+      fs.writeFile(path + '/map.json', JSON.stringify(json), () =>
+      {}
+    )
+  } else {
+    json = JSON.parse(fs.readFileSync(path + '/map.json'))
+  }
 
-total = Object.keys(json).length
-Object.keys(json).forEach(async (tokenAddress, i) => {
-  tokenAddress = tokenAddress.toLowerCase()
-  if (!fs.existsSync(path + '/' + tokenAddress + '.png')) {
-    timer += 1
-    setTimeout(async function() {
-      // console.log(tokenAddress)
-      await curly.get(chain.explorer + '/token/' + tokenAddress)
-      .then(({ statusCode, data }) => data)
-      .then(async (html) => {
-        const $ = cheerio.load(html)
+  total = Object.keys(json).length
+  Object.keys(json).forEach(async (tokenAddress, i) => {
+    tokenAddress = tokenAddress.toLowerCase()
+    if (!fs.existsSync(path + '/' + tokenAddress + '.png')) {
+      timer += 1
+      setTimeout(async function() {
+        // console.log(tokenAddress)
+        await curly.get(chain.explorer + '/token/' + tokenAddress)
+          .then(({ statusCode, data }) => data)
+          .then(async (html) => {
+            const $ = cheerio.load(html)
 
-        let imgSrc = ''
+            let imgSrc = ''
 
-        if($('img.u-sm-avatar').length) {
-          imgSrc = $('img.u-sm-avatar')[0].attribs.src
-        } else if($('img.js-token-avatar').length) {
-          imgSrc = $('img.js-token-avatar')[0].attribs.src
-        } else {
-          console.log('No picture found in the page for: ' + tokenAddress)
-          return
-        }
+            if($('img.u-sm-avatar').length) {
+              imgSrc = $('img.u-sm-avatar')[0].attribs.src
+            } else if($('img.js-token-avatar').length) {
+              imgSrc = $('img.js-token-avatar')[0].attribs.src
+            } else {
+              console.log('No picture found in the page for: ' + tokenAddress)
+              return
+            }
 
-        // console.log(imgSrc)
+            // console.log(imgSrc)
 
-        await curly.get(chain.explorer + imgSrc)
-        .then(({ statusCode, data }) => data)
-        .then(async (buffer) => {
+            await curly.get(chain.explorer + imgSrc)
+              .then(({ statusCode, data }) => data)
+              .then(async (buffer) => {
+                // console.log(path + '/' + tokenAddress + '.png')
+                fs.writeFile(path + '/' + tokenAddress + '.png', buffer, () => {
+                  imgDownloaded += 1
+                  logStatus()
+                })
+              })
+            })
+            .catch(error => {
+              console.log(tokenAddress, error)
+              errors += 1
+              logStatus()
+            })
+      }, timer * 1250)
 
-          // console.log(path + '/' + tokenAddress + '.png')
-          fs.writeFile(path + '/' + tokenAddress + '.png', buffer, () => {
-            imgDownloaded += 1
-            logStatus()
-          }
-        )
-      })
-    })
-    .catch(error => {
-      console.log(tokenAddress, error)
-      errors += 1
+    } else {
+      // already exists
+      alreadyExists += 1
       logStatus()
-    })
-  }, timer * 1250)
-
-} else {
-  // already exists
-  alreadyExists += 1
-  logStatus()
-}
-})
+    }
+  })
 
 })
 
